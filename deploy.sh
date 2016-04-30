7c938935bc178d036282fdef012ef5efef62ca35
Ersi Ni <ersi.ni@nilbot.net>
2016-04-30 17:43:18 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # I know I have sudo; so whatever
 sudo pkill quintet-ui
-go build -o quintet-ui main.go
-quintet-ui &>run.log &
+GOPATH=$HOME go build -o quintet-ui main.go
+./quintet-ui &>run.log &
 disown
 
