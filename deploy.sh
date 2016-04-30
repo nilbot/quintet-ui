cc3b9b8e61830d093702f879c5bb66d6506428a4
Ersi Ni <ersi.ni@nilbot.net>
2016-04-30 18:58:22 +0100

    Fix TLS still

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # I know I have sudo; so whatever
 sudo pkill quintet-ui
+openssl genrsa -out key 2048
+openssl req -new -x509 -key key -out crt -days 365
 GOPATH=$HOME go build -o quintet-ui main.go
 ./quintet-ui &>run.log &
 disown
